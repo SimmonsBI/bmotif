1d8972e25ab1a5be065f057349263af60da2cc29
Benno Simmons <benno.simmons@gmail.com>
2018-01-25 15:13:39 +0000

    Added transposed matrix to mcount test

--- tests/testthat/test_mcount.R
+++ tests/testthat/test_mcount.R
@@ -3,4 +3,9 @@ test_that("mcount behaves as expected",{
   expect_identical(object = mcount(M = mat, six_node = TRUE, normalise = FALSE), mcount_ST_NF)
   expect_identical(object = mcount(M = mat, six_node = FALSE, normalise = TRUE), mcount_SF_NT)
   expect_identical(object = mcount(M = mat, six_node = TRUE, normalise = TRUE), mcount_ST_NT)
+  expect_identical(mcount(M = t(mat), six_node = TRUE, normalise = TRUE), mcount_transpose_ST_NT)
+  expect_identical(mcount(M = t(mat), six_node = FALSE, normalise = TRUE), mcount_transpose_SF_NT)
+  expect_identical(mcount(M = t(mat), six_node = TRUE, normalise = FALSE), mcount_transpose_ST_NF)
+  expect_identical(mcount(M = t(mat), six_node = FALSE, normalise = FALSE), mcount_transpose_SF_NF)
+
 })

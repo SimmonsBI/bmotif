a465862a8060827af7a3f96f9979599a33330af8
Benno Simmons <benno.simmons@gmail.com>
2018-08-06 15:49:47 +0100

    If network has one link, sd of motif 1 is now zero rather than NA

--- R/pop_sd.r
+++ R/pop_sd.r
@@ -2,5 +2,9 @@ pop_sd <- function(v) {
   # compute population sd instead of sample sd
   # built in sd uses denominator n - 1
   n <- length(v)
-  return(stats::sd(v) * sqrt((n - 1) / n))
+  if(n == 1){
+    return(0)
+  } else {
+    return(stats::sd(v) * sqrt((n - 1) / n))
+  }
 }

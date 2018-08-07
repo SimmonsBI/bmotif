d3904669ef0e2941f2d5e1e9bb37a3580c8ec0a4
Benno Simmons <benno.simmons@gmail.com>
2018-08-07 12:06:49 +0100

    If a motif does not occur the SD is now NA not zero; reducing iterations of some tests to increase speed

--- tests/testthat/test_sd.R
+++ tests/testthat/test_sd.R
@@ -1,7 +1,6 @@
 context("sd")
-# library(testthat)
 
-# -------- define some functions
+# -------- Define some functions
 rbm <- function (m, n, one_prob = 0.5) {
   matrix(sample(0:1, m * n, replace = TRUE, prob = c(1- one_prob,one_prob)), m, n)
 }
@@ -24,7 +23,6 @@ motif_sd_minors <- function(W) {
   M <- W
   M[M > 0] <- 1
   mc <- mcount(M, FALSE, FALSE)$frequency
-  # library(gtools)
 
   # compute all_mot_perm
   all_mot_perm <- gen_all_mot_perm()
@@ -65,9 +63,11 @@ motif_sd_minors <- function(W) {
     }
   }
 
-  msd <- sqrt(zerodiv_vec(sd_sum, mc))
+  msd <- sqrt(sd_sum / mc)
+  msd <- replace(msd, which(is.nan(msd)), NA)
   msd
 }
+
 
 gen_all_mot_perm <- function(all_mot = -1) {
   # all_mot should be a list of all motifs that we want to permute
@@ -221,36 +221,65 @@ motif_dimensions <- function(m, n) {
 
 }
 
-# -------- tests
+# -------- Tests
 test_that("Testing for binary matrices", {
   W <- rbm(20,20)
+  mc <- mcount(W, FALSE, FALSE)$frequency
   msd <- motif_sd(W)
-  expect(all(msd == 0))
+  expect(all(msd[which(mc > 0)] == 0))
+  expect(all(is.na(msd[which(mc == 0)])))
+
   W <- rbm(5,5)
+  mc <- mcount(W, FALSE, FALSE)$frequency
   msd <- motif_sd_minors(W)
-  expect(all(msd == 0))
+  expect(all(msd[which(mc > 0)] == 0))
+  expect(all(is.na(msd[which(mc == 0)])))
+
+  for (i in 1:5) {
+    W <- rbm(3,3, 0.2) # make it sparse
+    if (any(W != 0)) {
+      mc <- mcount(W, FALSE, FALSE)$frequency
+      msd <- motif_sd_minors(W)
+      expect(all(msd[which(mc > 0)] == 0))
+      expect(all(is.na(msd[which(mc == 0)])))
+    }
+  }
 })
 
 test_that("Testing for matrices with equal weights", {
   W <- 2 * rbm(20,20)
   msd <- motif_sd(W)
-  expect(all(msd == 0))
+  mc <- mcount(W, FALSE, FALSE)$frequency
+  expect(all(msd[which(mc > 0)] == 0))
+  expect(all(is.na(msd[which(mc == 0)])))
 
   W <- 2 * rbm(5,5)
   msd <- motif_sd_minors(W)
-  expect(all(msd == 0))
+  mc <- mcount(W, FALSE, FALSE)$frequency
+  expect(all(msd[which(mc > 0)] == 0))
+  expect(all(is.na(msd[which(mc == 0)])))
 
   W <- 3.75 * rbm(20,20)
   msd <- motif_sd(W)
-  expect(all(msd == 0))
+  mc <- mcount(W, FALSE, FALSE)$frequency
+  expect(all(msd[which(mc > 0)] == 0))
+  expect(all(is.na(msd[which(mc == 0)])))
+})
 
+test_that("Testing matrix with single link", {
+  W <- matrix(0, 3, 3)
+  W[1,1] <- 1
+  msd <- motif_sd(W)
+  expect_equal(msd[1], 0)
+  expect(all(is.na(msd[2:17])))
 })
 
 test_that("Testing simple version of M2", {
   W <- matrix(c(1,2), nrow = 1)
   msd <- motif_sd(W)
   expect_equal(msd[1], pop_sd(c(1,2)))
-  expect_equal(msd[2:17], rep(0,16))
+  expect_equal(msd[2], 0)
+  expect(all(is.na(msd[3:17])))
 })
 
 
@@ -258,44 +287,51 @@ test_that("Testing simple version of M3", {
   W <- matrix(c(2.34,7.21), nrow = 2)
   msd <- motif_sd(W)
   expect_equal(msd[1], pop_sd(c(2.34,7.21)))
-  expect_equal(msd[2:3], c(0,0))
-  expect_equal(msd[4:17], rep(0,14))
+  expect(is.na(msd[2]))
+  expect_equal(msd[3], 0)
+  expect(all(is.na(msd[4:17])))
 
   msd <- motif_sd_minors(W)
   expect_equal(msd[1], pop_sd(c(2.34,7.21)))
-  expect_equal(msd[2:3], c(0,0))
-  expect_equal(msd[4:17], rep(0,14))
+  expect(is.na(msd[2]))
+  expect_equal(msd[3], 0)
+  expect(all(is.na(msd[4:17])))
 })
 
 test_that("Testing simple version of M4", {
   W <- matrix(c(5,7, 2), nrow = 3)
   msd <- motif_sd(W)
   expect_equal(msd[1], pop_sd(c(5,2,7)))
-  expect_equal(msd[2], 0)
+  expect(is.na(msd[2]))
   m3_weights <- c(mean(c(5,7)), mean(c(5,2)), mean(c(7,2)))
   expect_equal(msd[3], pop_sd(m3_weights))
-  expect_equal(msd[4:17], rep(0,14))
+  expect_equal(msd[4], 0)
+  expect(all(is.na(msd[5:17])))
 
   msd <- motif_sd_minors(W)
   expect_equal(msd[1], pop_sd(c(5,2,7)))
-  expect_equal(msd[2], 0)
+  expect(is.na(msd[2]))
   m3_weights <- c(mean(c(5,7)), mean(c(5,2)), mean(c(7,2)))
   expect_equal(msd[3], pop_sd(m3_weights))
-  expect_equal(msd[4:17], rep(0,14))
+  expect_equal(msd[4], 0)
+  expect(all(is.na(msd[5:17])))
 })
 
 test_that("Testing simple version of M5", {
   W <- matrix(c(5,7, 0, 2), nrow = 2)
   msd <- motif_sd(W)
   expect_equal(msd[1], pop_sd(c(5,2,7)))
-  expect_equal(msd[2:17], rep(0,16))
+  expect(all(msd[c(2,3,5)] == 0))
+  expect(all(is.na(msd[c(4,6:17)])))
 
   msd <- motif_sd_minors(W)
   expect_equal(msd[1], pop_sd(c(5,2,7)))
-  expect_equal(msd[2:17], rep(0,16))
+  expect(all(msd[c(2,3,5)] == 0))
+  expect(all(is.na(msd[c(4,6:17)])))
+
 })
 
-test_that("Testing simple version of M5", {
+test_that("Testing simple version of M9", {
   W <- matrix(c(5, 0, 7, 0, 2, 3), nrow = 3, byrow = 3)
   msd <- motif_sd(W)
   expect_equal(msd[1], pop_sd(c(5,2,7, 3)))
@@ -305,7 +341,9 @@ test_that("Testing simple version of M5", {
   expect_equal(msd[4], 0)
   m5_weights <- c(mean(c(5,2,3)), mean(c(7,2,3)))
   expect_equal(msd[5], pop_sd(m5_weights))
-  expect_equal(msd[9:17], rep(0, 9))
+  expect_equal(msd[9], 0)
+  expect(all(is.na(msd[6:8])))
+  expect(all(is.na(msd[10:17])))
 
   W <- matrix(c(5, 0, 7, 0, 2, 3), nrow = 3, byrow = TRUE)
   msd <- motif_sd_minors(W)
@@ -314,24 +352,26 @@ test_that("Testing simple version of M5", {
   expect_equal(msd[3], pop_sd(m3_weights))
   expect_equal(msd[4], 0)
   expect_equal(msd[5], pop_sd(m5_weights))
-  expect_equal(msd[9:17], rep(0, 9))
+  expect_equal(msd[9], 0)
+  expect(all(is.na(msd[6:8])))
+  expect(all(is.na(msd[10:17])))
 })
 
 
 test_that("Compare two versions", {
-  for (i in 1:10) {
+  for (i in 1:5) {
     W <- rwm(5,5, 0.6)
     msd_minors <- motif_sd_minors(W)
     msd <- motif_sd(W)
     expect_equal(msd_minors, msd)
   }
-  for (i in 1:10) {
+  for (i in 1:5) {
     W <- rwm(5,5, 0.7)
     msd_minors <- motif_sd_minors(W)
     msd <- motif_sd(W)
     expect_equal(msd_minors, msd)
   }
-  for (i in 1:10) {
+  for (i in 1:5) {
     W <- rwm(5,5, 0.8)
     msd_minors <- motif_sd_minors(W)
     msd <- motif_sd(W)
@@ -339,13 +379,13 @@ test_that("Compare two versions", {
   }
 })
 
-test_that("Test: If mcount is zero, sd should be zero", {
+test_that("Test: If mcount is zero, sd should be NA", {
   for (i in 1:10) {
     W <- rwm(10,10, 0.1)
     if(sum(W) != 0) {
       mc <- mcount (W, six_node = FALSE, normalisation = FALSE)$frequency
       msd <- motif_sd(W)
-      expect(all(msd[which(mc == 0)] == 0))
+      expect(all(is.na(msd[which(mc == 0)])))
     }
   }
 })

cbb8d32c9c5dcf92644837970aa72f285aee1c47
Benno Simmons <benno.simmons@gmail.com>
2018-08-07 12:01:21 +0100

    We now treat weights and SDs of motifs which don't occur in a network as NA rather than zero

--- R/zerodiv.R
+++
@@ -1,5 +0,0 @@
-zerodiv <- function(vec) {
-  x <- vec[1]
-  y <- vec[2]
-  ifelse(y == 0, return (0), return(x / y))
-}
--- R/zerodiv_vec.R
+++
@@ -1,4 +0,0 @@
-zerodiv_vec <- function(v,w) {
-  a <- cbind(v,w)
-  apply(a, 1, zerodiv)
-}

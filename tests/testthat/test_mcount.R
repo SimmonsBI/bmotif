6b13f40955addb15c5f39fcb5652938f3bc4dfa5
Benno Simmons <benno.simmons@gmail.com>
2018-08-14 11:52:41 +0100

    Corrected test names

--- tests/testthat/test_mcount.R
+++ tests/testthat/test_mcount.R
@@ -1,9 +1,14 @@
 context("mcount")
 
+# test against subgraph extraction method
+# rbm(m = , n = )
+# mcount_minors(W = m, six_node = FALSE)
+
+# tests with block matrices
 mlist2 <- motifs[1]
 bm2 <- block_matrix(mlist2)
 
-test_that("Test counts for the motifs with 4 nodes", {
+test_that("Test counts for the motifs with 2 nodes", {
   mc <- mcount(M = bm2, six_node = TRUE, normalisation = FALSE, mean_weight = FALSE, standard_dev = FALSE)
   expect_identical(all(mc[mc$nodes == 2,"frequency"] == 1), TRUE)
 })
@@ -11,7 +16,7 @@ test_that("Test counts for the motifs with 4 nodes", {
 mlist3 <- motifs[2:3]
 bm3 <- block_matrix(mlist3)
 
-test_that("Test counts for the motifs with 4 nodes", {
+test_that("Test counts for the motifs with 3 nodes", {
   mc <- mcount(M = bm3, six_node = TRUE, normalisation = FALSE, mean_weight = FALSE, standard_dev = FALSE)
   expect_identical(all(mc[mc$nodes == 3,"frequency"] == 1), TRUE)
 })
@@ -41,13 +46,17 @@ test_that("Test counts for the motifs with 6 nodes", {
   expect_identical(all(mc[mc$nodes == 6,"frequency"] == 1), TRUE)
 })
 
-test_that("mcount behaves as expected",{
+# error messages
+test_that("Error messages work",{
   expect_error(object = mcount("a"), "'M' must be an object of class 'matrix'")
   expect_error(object = mcount(matrix("a",3,3)), "Elements of 'M' must be numeric")
   expect_error(object = mcount(matrix(-1,3,3)), "Elements of 'M' must be greater than or equal to zero")
   expect_error(object = mcount(matrix(1,3,3), six_node = TRUE, normalisation = 7), "'normalisation' must be of class 'logical' i.e. TRUE or FALSE")
   expect_error(object = mcount(matrix(1,3,3), six_node = 7, normalisation = TRUE), "'six_node' must be of class 'logical' i.e. TRUE or FALSE")
+})
 
+# validated results
+test_that("Matches validated results",{
   expect_equal(object = mcount(M = mat, six_node = FALSE, normalisation = FALSE, mean_weight = FALSE, standard_dev = FALSE), mcount_SF_NF)
   expect_equal(object = mcount(M = mat, six_node = TRUE, normalisation = FALSE, mean_weight = FALSE, standard_dev = FALSE), mcount_ST_NF)
   expect_equal(object = mcount(M = mat, six_node = FALSE, normalisation = TRUE, mean_weight = FALSE, standard_dev = FALSE), mcount_SF_NT)

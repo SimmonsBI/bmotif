7d18c5c0bc7b962a75ae66c66198702a6af262cf
Benno Simmons <benno.simmons@gmail.com>
2018-08-16 17:48:20 +0100

    Pass six node argument to normalise_node_positions

--- tests/testthat/test_normalise_node_positions.R
+++ tests/testthat/test_normalise_node_positions.R
@@ -3,22 +3,22 @@ context("normalise_node_positions")
 pc <- matrix(1, 10, 148, dimnames = list(c(paste0("r",1:5),paste0("c",1:5)),paste0("np",1:148)))
 
 test_that("'none' doesn't change anything",{
-  nnp <- normalise_node_positions(pc = pc, type = "none")
+  nnp <- normalise_node_positions(pc = pc, type = "none", six_node = TRUE)
   expect_identical(pc, nnp)
 })
 
 test_that("'sum' works correctly when pc is all 1s",{
-  nnp <- normalise_node_positions(pc = pc, type = "sum")
+  nnp <- normalise_node_positions(pc = pc, type = "sum", six_node = TRUE)
   expect_identical(all(rowSums(nnp) == 1), TRUE)
 })
 
 test_that("'position' works correctly when pc is all 1s",{
-  nnp <- normalise_node_positions(pc = pc, type = "position")
+  nnp <- normalise_node_positions(pc = pc, type = "position", six_node = TRUE)
   expect_identical(all(colSums(nnp) == 1), TRUE)
 })
 
 test_that("'sizeclass' works correctly when pc is all 1s",{
-  nnp <- normalise_node_positions(pc = pc, type = "sizeclass")
+  nnp <- normalise_node_positions(pc = pc, type = "sizeclass", six_node = TRUE)
   expect_identical(all(rowSums(nnp[,1:2]) == 1), TRUE)
   expect_identical(all(rowSums(nnp[,3:6]) == 1), TRUE)
   expect_identical(all(rowSums(nnp[,7:16]) == 1), TRUE)
@@ -27,7 +27,7 @@ test_that("'sizeclass' works correctly when pc is all 1s",{
 })
 
 test_that("'levelsize' works correctly when pc is all 1s",{
-  nnp <- normalise_node_positions(pc = pc, type = "levelsize")
+  nnp <- normalise_node_positions(pc = pc, type = "levelsize", six_node = TRUE)
   expect_identical(all(rowSums(nnp[,1:2]) == 1), TRUE)
   expect_identical(all(rowSums(nnp[,3:4]) == 1), TRUE)
   expect_identical(all(rowSums(nnp[,5:6]) == 1), TRUE)
@@ -46,7 +46,7 @@ test_that("'levelsize' works correctly when pc is all 1s",{
 })
 
 test_that("'motif' works correctly when pc is all 1s",{
-  nnp <- normalise_node_positions(pc = pc, type = "motif")
+  nnp <- normalise_node_positions(pc = pc, type = "motif", six_node = TRUE)
   mps <- lapply(1:44, function(x) motif_info(x, link = FALSE)) # motif positions
   for(i in mps){
     expect_identical(all(rowSums(nnp[,i]) == 1), TRUE)
@@ -58,8 +58,8 @@ test_that("'sizeclass_NAzero' replaces all NAs/NaNs with zero",{
   for(i in 1:10){
     m <- motifs[[sample(17,size = 1)]]
     np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
-    np_normal <- normalise_node_positions(np, type = "sizeclass")
-    np_NAzero <- normalise_node_positions(np, type = "sizeclass_NAzero")
+    np_normal <- normalise_node_positions(np, type = "sizeclass", six_node = TRUE)
+    np_NAzero <- normalise_node_positions(np, type = "sizeclass_NAzero", six_node = TRUE)
     expect_identical(all(np_NAzero[is.na(np_normal)] == 0), TRUE)
   }
 })
@@ -68,8 +68,8 @@ test_that("'levelsize_NAzero' replaces all NAs/NaNs with zero",{
   for(i in 1:10){
     m <- motifs[[sample(44,size = 1)]]
     np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
-    np_normal <- normalise_node_positions(np, type = "levelsize")
-    np_NAzero <- normalise_node_positions(np, type = "levelsize_NAzero")
+    np_normal <- normalise_node_positions(np, type = "levelsize", six_node = TRUE)
+    np_NAzero <- normalise_node_positions(np, type = "levelsize_NAzero", six_node = TRUE)
     expect_identical(all(np_NAzero[is.na(np_normal)] == 0), TRUE)
   }
 })
@@ -78,8 +78,8 @@ test_that("'motif_NAzero' replaces all NAs/NaNs with zero",{
   for(i in 1:10){
     m <- motifs[[sample(44,size = 1)]]
     np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
-    np_normal <- normalise_node_positions(np, type = "motif")
-    np_NAzero <- normalise_node_positions(np, type = "motif_NAzero")
+    np_normal <- normalise_node_positions(np, type = "motif", six_node = TRUE)
+    np_NAzero <- normalise_node_positions(np, type = "motif_NAzero", six_node = TRUE)
     expect_identical(all(np_NAzero[is.na(np_normal)] == 0), TRUE)
   }
 })
@@ -89,8 +89,8 @@ test_that("'sizeclass_plus1' removes all NAs/NaNs",{
   for(i in 1:10){
     m <- motifs[[sample(17,size = 1)]]
     np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
-    np_normal <- normalise_node_positions(np, type = "sizeclass")
-    np_NAzero <- normalise_node_positions(np, type = "sizeclass_plus1")
+    np_normal <- normalise_node_positions(np, type = "sizeclass", six_node = TRUE)
+    np_NAzero <- normalise_node_positions(np, type = "sizeclass_plus1", six_node = TRUE)
     expect_identical(all(!is.na(np_NAzero[is.na(np_normal)])), TRUE)
   }
 })
@@ -99,8 +99,8 @@ test_that("'levelsize_plus1' removes all NAs/NaNs",{
   for(i in 1:10){
     m <- motifs[[sample(44,size = 1)]]
     np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
-    np_normal <- normalise_node_positions(np, type = "levelsize")
-    np_NAzero <- normalise_node_positions(np, type = "levelsize_plus1")
+    np_normal <- normalise_node_positions(np, type = "levelsize", six_node = TRUE)
+    np_NAzero <- normalise_node_positions(np, type = "levelsize_plus1", six_node = TRUE)
     expect_identical(all(!is.na(np_NAzero[is.na(np_normal)])), TRUE)
   }
 })
@@ -109,8 +109,8 @@ test_that("'motif_plus1' removes all NAs/NaNs",{
   for(i in 1:10){
     m <- motifs[[sample(44,size = 1)]]
     np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
-    np_normal <- normalise_node_positions(np, type = "motif")
-    np_NAzero <- normalise_node_positions(np, type = "motif_plus1")
+    np_normal <- normalise_node_positions(np, type = "motif", six_node = TRUE)
+    np_NAzero <- normalise_node_positions(np, type = "motif_plus1", six_node = TRUE)
     expect_identical(all(!is.na(np_NAzero[is.na(np_normal)])), TRUE)
   }
 })

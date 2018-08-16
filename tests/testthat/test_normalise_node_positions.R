49da4f25c91de6664ffa308c7c9ad90fa81cbc54
Benno Simmons <benno.simmons@gmail.com>
2018-08-16 16:59:06 +0100

    More unit tests for normalise_node_positions

--- tests/testthat/test_normalise_node_positions.R
+++ tests/testthat/test_normalise_node_positions.R
@@ -52,3 +52,67 @@ test_that("'motif' works correctly when pc is all 1s",{
     expect_identical(all(rowSums(nnp[,i]) == 1), TRUE)
   }
 })
+
+### ------ Testing _NAzero
+test_that("'sizeclass_NAzero' replaces all NAs/NaNs with zero",{
+  for(i in 1:10){
+    m <- motifs[[sample(17,size = 1)]]
+    np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
+    np_normal <- normalise_node_positions(np, type = "sizeclass")
+    np_NAzero <- normalise_node_positions(np, type = "sizeclass_NAzero")
+    expect_identical(all(np_NAzero[is.na(np_normal)] == 0), TRUE)
+  }
+})
+
+test_that("'levelsize_NAzero' replaces all NAs/NaNs with zero",{
+  for(i in 1:10){
+    m <- motifs[[sample(44,size = 1)]]
+    np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
+    np_normal <- normalise_node_positions(np, type = "levelsize")
+    np_NAzero <- normalise_node_positions(np, type = "levelsize_NAzero")
+    expect_identical(all(np_NAzero[is.na(np_normal)] == 0), TRUE)
+  }
+})
+
+test_that("'motif_NAzero' replaces all NAs/NaNs with zero",{
+  for(i in 1:10){
+    m <- motifs[[sample(44,size = 1)]]
+    np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
+    np_normal <- normalise_node_positions(np, type = "motif")
+    np_NAzero <- normalise_node_positions(np, type = "motif_NAzero")
+    expect_identical(all(np_NAzero[is.na(np_normal)] == 0), TRUE)
+  }
+})
+
+### ------ Testing _plus1
+test_that("'sizeclass_plus1' removes all NAs/NaNs",{
+  for(i in 1:10){
+    m <- motifs[[sample(17,size = 1)]]
+    np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
+    np_normal <- normalise_node_positions(np, type = "sizeclass")
+    np_NAzero <- normalise_node_positions(np, type = "sizeclass_plus1")
+    expect_identical(all(!is.na(np_NAzero[is.na(np_normal)])), TRUE)
+  }
+})
+
+
+
+test_that("'levelsize_plus1' removes all NAs/NaNs",{
+  for(i in 1:10){
+    m <- motifs[[sample(44,size = 1)]]
+    np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
+    np_normal <- normalise_node_positions(np, type = "levelsize")
+    np_NAzero <- normalise_node_positions(np, type = "levelsize_plus1")
+    expect_identical(all(!is.na(np_NAzero[is.na(np_normal)])), TRUE)
+  }
+})
+
+test_that("'motif_plus1' removes all NAs/NaNs",{
+  for(i in 1:10){
+    m <- motifs[[sample(44,size = 1)]]
+    np <- node_positions(M = m, six_node = TRUE, level = "all", weights_method = "none", weights_combine = "none", normalisation = "none")
+    np_normal <- normalise_node_positions(np, type = "motif")
+    np_NAzero <- normalise_node_positions(np, type = "motif_plus1")
+    expect_identical(all(!is.na(np_NAzero[is.na(np_normal)])), TRUE)
+  }
+})
